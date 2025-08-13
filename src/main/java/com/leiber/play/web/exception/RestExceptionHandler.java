ebd0310ab547343bde9876a60ff91156fd207e3e
leiber bertel <leiberbertel777@gmail.com>
2025-08-12 23:13:19 -0500

    feat: add methods to capture exceptions raised by validations and to capture general errors

--- src/main/java/com/leiber/play/web/exception/RestExceptionHandler.java
+++ src/main/java/com/leiber/play/web/exception/RestExceptionHandler.java
@@ -3,8 +3,12 @@ package com.leiber.play.web.exception;
 import com.leiber.play.domain.exception.MovieAlreadyExistsException;
 import com.leiber.play.domain.exception.MovieNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -19,5 +23,22 @@ public class RestExceptionHandler {
     public ResponseEntity<Error> handleException(MovieNotFoundException e) {
         Error error = new Error("movie-not-found", e.getMessage());
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handleException(MethodArgumentNotValidException e) {
+        List<Error> errors = new ArrayList<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.add(new Error(error.getField(), error.getDefaultMessage()));
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(Exception e) {
+        Error error = new Error("unknown-error", e.getMessage());
+        return ResponseEntity.internalServerError().body(error);
     }
 }
