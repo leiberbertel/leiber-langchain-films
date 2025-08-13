65eb235f003e1d01ddbc4787d7cedde714325212
leiber bertel <leiberbertel777@gmail.com>
2025-08-13 00:52:59 -0500

    docs: add documentation for the Swagger services for movies and hello

--- src/main/java/com/leiber/play/web/controller/HelloController.java
+++ src/main/java/com/leiber/play/web/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.leiber.play.web.controller;
 
 import com.leiber.play.domain.service.LeiberPlayAiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Hola", description = "Operaciones de saludo al usuario")
 public class HelloController {
 
     private final String platform;
@@ -17,6 +20,9 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
+    @Operation(
+            summary = "Retorna una cadena con un saludo divertido con el nombre del sistema"
+    )
     public String hello() {
         return this.leiberPlayAiService.generateGreeting(platform);
     }
--- src/main/java/com/leiber/play/web/controller/MovieController.java
+++ src/main/java/com/leiber/play/web/controller/MovieController.java
@@ -5,6 +5,11 @@ import com.leiber.play.domain.dto.SuggestRequestDto;
 import com.leiber.play.domain.dto.UpdateMovieDto;
 import com.leiber.play.domain.service.LeiberPlayAiService;
 import com.leiber.play.domain.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -15,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
+@Tag(name = "Peliculas", description = "Operaciones relacionadas con las películas de LeiberFilms AI")
 public class MovieController {
 
     private final String platform;
@@ -30,13 +36,31 @@ public class MovieController {
     }
 
     @GetMapping()
+    @Operation(
+            summary = "Obtiene todas las peliculas del sistema",
+            description = "Retorna todas las peliculas del sistema",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+            }
+    )
     public ResponseEntity<List<MovieDto>> getAll() {
         return ResponseEntity
                 .ok(this.movieService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<MovieDto> getById(@PathVariable Long id) {
+    @Operation(
+            summary = "Obtiene una película por su identificador",
+            description = "Retorna la película que coincida con el identificador enviado",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Película encontrada"),
+                    @ApiResponse(responseCode = "404", description = "Película no encontrada", content = @Content)
+            }
+    )
+    public ResponseEntity<MovieDto> getById(
+            @Parameter(description = "Identificador de la película a obtener",
+                    example = "9")
+            @PathVariable Long id) {
         MovieDto movieDto = this.movieService.getById(id);
         if (movieDto == null) {
             return ResponseEntity
@@ -48,6 +72,14 @@ public class MovieController {
     }
 
     @PostMapping()
+    @Operation(
+            summary = "Guarda una nueva película",
+            description = "Guarda una nueva película si cumple las condiciones del esquema Movie",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Película registrada"),
+                    @ApiResponse(responseCode = "400", description = "La película ya existe", content = @Content)
+            }
+    )
     public ResponseEntity<MovieDto> save(@RequestBody @Valid MovieDto movie) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -55,19 +87,43 @@ public class MovieController {
     }
 
     @PostMapping("/suggest")
+    @Operation(
+            summary = "Retorna 3 sugerencias de peliculas",
+            description = "Sugiere 3 peliculas registradas en el sistema al usuario de acuerdo a las preferencias enviadas",
+            responses = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "400", description = "Las preferencias son obligatorias", content = @Content)
+            }
+    )
     public ResponseEntity<String> generateMoviesSuggestion(@RequestBody @Valid SuggestRequestDto suggestRequestDto) {
         return ResponseEntity
                 .ok(this.leiberPlayAiService.
-                        generateMoviesSuggestion(platform,suggestRequestDto.userPreferences()));
+                        generateMoviesSuggestion(platform, suggestRequestDto.userPreferences()));
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Actualiza una película por su id",
+            description = "Actualiza los campos de title, releaseDate y rating de un película",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Película actualizada"),
+                    @ApiResponse(responseCode = "400", description = "La película no existe", content = @Content)
+            }
+    )
     public ResponseEntity<MovieDto> update(@PathVariable Long id, @RequestBody @Valid UpdateMovieDto movie) {
         return ResponseEntity
                 .ok(this.movieService.update(id, movie));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Elimina una película por su id",
+            description = "Si el borrado es exitoso retorna la entidad borrada",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Película eliminada"),
+                    @ApiResponse(responseCode = "400", description = "La película no existe", content = @Content)
+            }
+    )
     public ResponseEntity<MovieDto> delete(@PathVariable Long id) {
         return ResponseEntity
                 .ok(this.movieService.delete(id));
